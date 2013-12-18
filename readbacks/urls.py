16222d4f8106736d38908571876aa2a28da1d6ac
Alexander Tokar <alext@bitbamboo.com>
2013-12-18 14:32:25 -0800

    homepage app now complete

--- readbacks/urls.py
+++ readbacks/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'readbacks.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^', include('readbacks.apps.home.urls')),
+    # url(r'^reader/', include('apps.reader.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
