fbe89aef3ff7a0ee59f08a0ecfc4bb979b66e16d
tiannak <sunstance@gmail.com>
2014-01-31 14:44:17 -0800

    created  a list view for grades in urls

--- readbacks/apps/reader/urls.py
+++ readbacks/apps/reader/urls.py
@@ -1,10 +1,24 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+#, DetailView
+from readbacks.apps.reader.models import Grade
 
+#  Do we change templateview to listview as seen in 2nd video?
+#  (see url listview.as_view commented out below)     
+#  NEXT ADD IN CODE in grades.html file for PAGINATION?
 urlpatterns = patterns('',
+#   url(r'^$', 
+#    detailview.as_view(context_object_name="Grades"
+#    model = Grades,    
+#    template_name='reader/grades.html'),
+   
     url(
         r'^$',
-        TemplateView.as_view(template_name='reader/grades.html'),
+        ListView.as_view(
+            model = Grade,
+            context_object_name = 'grades',
+            template_name='reader/grades.html'
+        ),
         name='grades'
     ),
     url(
